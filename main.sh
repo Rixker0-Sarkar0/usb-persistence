1442780cb9d38c3a21beb6c0e63cb658f908dde2
Rixker0-Sarkar0 <85361366+Rixker0-Sarkar0@users.noreply.github.com>
2022-07-30 13:22:11 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,9 @@
+echo enter name storage of drive
+read usb
+fdisk $usb <<< $(printf "n\np\n\n\n\nw")
+mkfs.ext4 -L persistence ${usb}3
+mkdir -p /mnt/my_usb
+mount ${usb}3 /mnt/my_usb
+echo "/ union" | sudo tee /mnt/my_usb/persistence.conf
+umount ${usb}3
+exit
