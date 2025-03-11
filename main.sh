73fdbc218d1c0f44197a593cd00fb64292a5218b
Rixker0-Sarkar0 <85361366+Rixker0-Sarkar0@users.noreply.github.com>
2025-03-11 07:49:54 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 fdisk -l
-echo enter name of drive
+echo "Enter the name of the drive (e.g., /dev/sdb):"
 read usb
-fdisk $usb <<< $(printf "n\np\n\n\n\nw")
-mkfs.ext4 -L persistence ${usb}3
+echo -e "o\nn\np\n1\n\n+512M\nn\np\n2\n\n\nw" | fdisk "$usb"
+mkfs.ext4 -L persistence "${usb}1"
+mkfs.ext4 -L iso "${usb}2"
 mkdir -p /mnt/my_usb
-mount ${usb}3 /mnt/my_usb
+mount "${usb}1" /mnt/my_usb
 echo "/ union" | sudo tee /mnt/my_usb/persistence.conf
-umount ${usb}3
+umount "${usb}1"
 exit
